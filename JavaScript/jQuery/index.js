cab522891c8cd4ad0e872cef59d04ec453c08241
Nasser <nassertariqalem@gmial.com>
2025-11-19 22:59:38 +0300

    keypress

--- JavaScript/jQuery/index.js
+++ JavaScript/jQuery/index.js
@@ -1,4 +1,11 @@
-$(document).no("keyprass",function(event) {
+// $(document).keypress(function(event) {
+    
+//     $("h1").text(event.key);
+//     $("title").text(event.key);
+// });
+
+$(document).on("keypress",function(event) {
+    
     $("h1").text(event.key);
     $("title").text(event.key);
 });
