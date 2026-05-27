0a3aa052beca94b06c01f269079de3c54523ab0c
Nasser-DataSaurus <nassertariqalem@gmail.com>
2026-05-27 23:11:17 +0300

    Add EJS templates for Band Generator project
    - Created index.ejs for the main view with a form to generate band names.
    - Added header.ejs for the HTML structure and linked CSS stylesheet.
    - Implemented footer.ejs with a static copyright notice.
    - Developed solution-header.ejs and solution-footer.ejs for the solution view.
    - Created solution.ejs to display the generated band name or a welcome message based on user input.

--- 4.Node/4.0 EJS/index.js
+++ 4.Node/4.0 EJS/index.js
@@ -10,27 +10,52 @@ const port = 3000;
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-function dayChecker(req, res, next) {
-  const today = new Date();
-  const dayIndex = today.getDay();
+// function dayChecker(req, res, next) {
+//   const today = new Date();
+//   const dayIndex = today.getDay();
 
-  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  const isWeekend = dayIndex === 6 || dayIndex === 5;
+//   const isWeekend = dayIndex === 6 || dayIndex === 5;
 
-  res.locals.dayName = dayNames[dayIndex];
-  res.locals.week = isWeekend ? 'Weekend' : 'Weekday';
-  res.locals.activity = isWeekend ? 'Have fun!' : 'Work hard!';
+//   res.locals.dayName = dayNames[dayIndex];
+//   res.locals.week = isWeekend ? 'Weekend' : 'Weekday';
+//   res.locals.activity = isWeekend ? 'Have fun!' : 'Work hard!';
 
-  next();
-}
+//   next();
+// }
 
 
-app.use(dayChecker);
+//app.use(dayChecker);
 
 app.get('/', (req, res) => {
-  res.render(__dirname + '/views/index.ejs');
-});
+  const today = new Date();
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  // console.log(day);
+  let week = "a weekday";
+  let activity = "it's time to work hard";
+  let dayName = dayNames[day];
+
+  if (day === 5 || day === 6) {
+    week = "the weekend";
+    activity = "it's time to have some fun";
+  }
+
+  res.render(__dirname + '/views/index.ejs'
+    , {
+    week: week,
+    activity: activity,
+    dayName: dayName
+  });
+
+  });
+
 
 
 
