42e5be07aa797e7e1cac15cf03ce334ace681c52
Nasser-DataSaurus <nassertariqalem@gmail.com>
2026-05-29 15:14:29 +0300

    How to use the API and Error handling

--- 5.API/5.3 Axios/index.js
+++ 5.API/5.3 Axios/index.js
@@ -30,6 +30,21 @@ app.post("/", async (req, res) => {
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
+  const type = req.body.type;
+  const participants = req.body.participants;
+  console.log(type, participants);
+    try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+    const randomIndex = Math.floor(Math.random() * response.data.length);
+    const result = response.data[randomIndex];
+    console.log(result);
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
